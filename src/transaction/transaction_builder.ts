88aaf0d2ee73549676bd298430a5b269053376fc
canguruhh <sven@mutzl.org>
2020-04-15 16:54:33 +0200

    Transaction builder sendmore

--- src/transaction/transaction_builder.ts
+++ src/transaction/transaction_builder.ts
@@ -31,7 +31,26 @@ export interface TransactionBuilderSendParams {
     asset_change_address?: string
 }
 
+export interface TransactionBuilderRecipient {
+    target: TransactionTarget,
+    address: string,
+    avatar?: string,
+    attenuation_model?: string,
+}
+
+export interface TransactionBuilderSendMoreParams {
+    inputs: UTXO[]
+    recipients: TransactionBuilderRecipient[]
+    etp_change_address: string
+    asset_change_address?: string
+    change: TransactionTarget
+    locked_asset_change?: AttenuationParams[]
+    fee?: number
+    messages?: string[]
+}
+
 export class TransactionBuilder {
+
     /**
      * Generates a send (etp and or asset) transaction with the given utxos as inputs, assets and the change.
      * @param inputs Inputs for the transaction
@@ -93,5 +112,65 @@ export class TransactionBuilder {
         }
         if (etpcheck !== params.fee) throw Error('ERR_FEE_CHECK_FAILED')
         return tx
-    };
+    }
+
+    static sendMore(params: TransactionBuilderSendMoreParams) {
+
+        if(params.fee === undefined) params.fee = FEE_DEFAULT
+        if (params.messages == undefined) {
+            params.messages = []
+        }
+
+        let etpcheck = 0
+        //create new transaction
+        let tx = new Transaction()
+        //add inputs
+        params.inputs.forEach((utxo) => {
+            if (utxo.value) {
+                etpcheck += utxo.value
+            }
+            tx.inputs.push(new Input(utxo.tx, utxo.index))
+        })
+        
+        params.messages.forEach((message) => tx.outputs.push(new OutputMessage(message, params.recipients[0].address)))
+        //add the target outputs to the recipient
+        params.recipients.forEach(recipient=>{
+            Object.keys(recipient.target).forEach(symbol=>{
+                let output
+                if (symbol.toUpperCase() === 'ETP') {
+                    etpcheck -= recipient.target.ETP
+                    output = new OutputETPTransfer(recipient.address, recipient.target[symbol])
+                } else {
+                    output = new OutputMSTTransfer(recipient.address, symbol, recipient.target[symbol])
+                }
+                if (recipient.avatar) {
+                    output.attachment.setDid("", recipient.avatar)
+                }
+                if(recipient.attenuation_model){
+                    throw Error('Attenuation models not supported')
+                }
+                tx.outputs.push(output)
+            })
+        })
+        //add the change outputs
+        Object.keys(params.change).forEach((symbol) => {
+            if (params.change[symbol] !== 0) {
+                if (symbol.toUpperCase() === 'ETP') {
+                    tx.outputs.push(new OutputETPTransfer(params.etp_change_address, -params.change[symbol]))
+                } else {
+                    tx.outputs.push(new OutputMSTTransfer(params.asset_change_address || params.etp_change_address, symbol, -params.change[symbol]))
+                }
+            }
+        })
+        if (params.locked_asset_change != undefined && params.locked_asset_change.length) {
+            throw Error('Attenuation models not supported')
+            // locked_asset_change.forEach((change) => tx.outputs.push(new OutputMSTTransfer(asset_change_address, change.symbol, change.quantity, change.attenuation_model, change.delta, change.hash, change.index));
+        }
+        if (params.change.ETP){
+            etpcheck += params.change.ETP
+        }
+        if (etpcheck !== params.fee) throw Error('ERR_FEE_CHECK_FAILED')
+        return tx
+    }
+
 }
